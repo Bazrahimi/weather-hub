d28d44aec8008880dafbe46e38a9d8ad72d31fdd
Baz Rahimi <bazrahimi@hotmail.com>
2023-07-18 23:35:45 +1000

    append wind and humidity to forecasts

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -29,7 +29,9 @@ function fetchApi(latitude, longitude) {
 // Function to render weather data
 function renderWeatherData(data) {
   const city = data.city.name;
-  const mainWeather = data.list[0].weather[0].main;
+  const countryCode = data.city.country;
+  const weatherIconCode = data.list[0].weather[0].icon;
+  const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
   const weatherDescription = data.list[0].weather[0].description;
   const kelvin = data.list[0].main.temp;
   const windSpeedMetersPerSec = data.list[0].wind.speed;
@@ -44,7 +46,13 @@ function renderWeatherData(data) {
 
   const currentDate = new Date().toLocaleDateString();
 
-  cityPlusDate.textContent = `${city} (${currentDate}) ${mainWeather} ${weatherDescription}`;
+  //create weather Icon element
+  const weatherIconEl = document.createElement('img');
+  weatherIconEl.src = weatherIconUrl;
+  weatherIconEl.alt = weatherDescription;
+
+  cityPlusDate.textContent = `${city}, ${countryCode} (${currentDate}) ${weatherDescription}`;
+  cityPlusDate.appendChild(weatherIconEl);
   tempDiv.textContent = "Temperature: " + temperature + "\u00B0C";
   windDiv.textContent = "Wind: " + windSpeed + " km/h";
   humidityDiv.textContent = `Humidity: ${humidity} %`;
@@ -62,9 +70,23 @@ function renderForecastData(data) {
     const dateTime = forecastItem.dt_txt;
     const date = new Date(dateTime).toLocaleDateString();
     const weatherDescription = forecastItem.weather[0].description;
+    const weatherIconCode = forecastItem.weather[0].icon;
+    const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
     const kelvin = forecastItem.main.temp;
+    const windSpeedMetersPerSec = forecastItem.wind.speed;
+    const humidity = forecastItem.main.humidity;
+ 
+    //convert kelvin to degreeCelsius
     const degreeCelsius = kelvin - 273.15;
-    const temperature = Math.round(degreeCelsius);
+    const temperature = Math.round(degreeCelsius); 
+
+    // Convert Wind Meters per Sec to Km/h and round off
+    const windSpeed = Math.round(windSpeedMetersPerSec * 3.6);
+
+    // Create the weather icon element
+    const weatherIconEl = document.createElement('img');
+    weatherIconEl.src = weatherIconUrl;
+    weatherIconEl.alt = weatherDescription;
 
     // Create a forecast item element
     const forecastItemEl = document.createElement('div');
@@ -73,8 +95,11 @@ function renderForecastData(data) {
     // Populate the forecast item element with data
     forecastItemEl.innerHTML = `
       <h5>${date}</h5>
+      <p>${weatherIconEl.outerHTML}</p>
       <p>${weatherDescription}</p>
-      <p>Temperature: ${temperature}°C</p>`;
+      <p>Temp: ${temperature}°C</p>
+      <p>Wind: ${windSpeed} km/h</p>
+      <p>Humidity: ${humidity}%</p>`;
 
     // Append the forecast item element to the forecast container
     forecastDiv.appendChild(forecastItemEl);
@@ -122,10 +147,10 @@ searchBtn.addEventListener('click', function() {
 });
 
 //set Melbourne as the default city
-document.addEventListener('DOMContentLoaded', () => {
-  const latitude = '-37.8142';
-  const longitude = '144.9632';
-  fetchApi(latitude, longitude)
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const latitude = '-37.8142';
+//   const longitude = '144.9632';
+//   fetchApi(latitude, longitude)
+// });
 
 
