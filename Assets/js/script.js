8dbf669b63418affeeb099a4ee27f97d8439b7b5
Baz Rahimi <bazrahimi@hotmail.com>
2023-07-18 12:49:51 +1000

    add css

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,7 +6,7 @@ const cityPlusDate = document.getElementById('cityPlusDate');
 const tempDiv = document.getElementById('temp');
 const windDiv = document.getElementById('wind');
 const humidityDiv = document.getElementById('humidity');
-const forcastDiv = document.getElementById('days');
+const forecastDiv = document.getElementById('days');
 
 // Add API Key
 const apiKey = "9d289b87c3721d7d57fae4326e6dad30";
@@ -21,60 +21,67 @@ function fetchApi(latitude, longitude) {
     .then(function(data) {
       console.log(data);
       renderWeatherData(data);
-      renderForcastData(data);
+      renderForecastData(data);
     });
 }
 
 // Function to render weather data
 function renderWeatherData(data) {
   const city = data.city.name;
+  const mainWeather = data.list[0].weather[0].main;
+  const weatherDescription = data.list[0].weather[0].description;
   const kelvin = data.list[0].main.temp;
   const windSpeedMetersPerSec = data.list[0].wind.speed;
   const humidity = data.list[0].main.humidity;
 
-  //convert Kelvin to Degree Celsius and round off
+  // Convert Kelvin to Degree Celsius and round off
   const degreeCelsius = kelvin - 273.15;
   const temperature = Math.round(degreeCelsius);
 
-  //convert Wind Meters per Sec to Km/h and round off
+  // Convert Wind Meters per Sec to Km/h and round off
   const windSpeed = Math.round(windSpeedMetersPerSec * 3.6);
-
 
   const currentDate = new Date().toLocaleDateString();
 
-  cityPlusDate.textContent = `${city} (${currentDate})`;
+  cityPlusDate.textContent = `${city} (${currentDate}) ${mainWeather} ${weatherDescription}`;
   tempDiv.textContent = "Temperature: " + temperature + "\u00B0C";
-
-
   windDiv.textContent = "Wind: " + windSpeed + " km/h";
   humidityDiv.textContent = `Humidity: ${humidity} %`;
 
   selectedCityWeather.style.display = "block";
 }
 
-//function to render 5 Days forecast
-function renderForcastData(data) {
-  const forcastList = data.list;
-  forcastDiv.innerHTML = '';
+// Function to render 5-day forecast weather
+function renderForecastData(data) {
+  const forecastItems = data.list.filter((item, index) => [6, 14, 22, 30, 38].includes(index));
 
-  for (let i = 0; i < forecastList.length; i++) {
-    const forecastData = forecastList[i];
-    const dateTime = forecastData.dt_txt;
-    const kelvin = forecastData.main.temp;
+  forecastDiv.innerHTML = ""; // Clear previous forecast data
 
-    // Convert Kelvin to Degree Celsius and round off
+  forecastItems.forEach(forecastItem => {
+    const dateTime = forecastItem.dt_txt;
+    const date = new Date(dateTime).toLocaleDateString();
+    const weatherDescription = forecastItem.weather[0].description;
+    const kelvin = forecastItem.main.temp;
     const degreeCelsius = kelvin - 273.15;
     const temperature = Math.round(degreeCelsius);
 
-    const forecastItem = document.createElement("div");
-    forecastItem.textContent = `${dateTime}: ${temperature}\u00B0C`;
-    forcastDiv.appendChild(forecastItem);
-  }
+    // Create a forecast item element
+    const forecastItemEl = document.createElement('div');
+    forecastItemEl.classList.add('forecast-item');
+
+    // Populate the forecast item element with data
+    forecastItemEl.innerHTML = `
+      <h5>${date}</h5>
+      <p>${weatherDescription}</p>
+      <p>Temperature: ${temperature}°C</p>`;
+
+    // Append the forecast item element to the forecast container
+    forecastDiv.appendChild(forecastItemEl);
+  });
 }
 
 searchBtn.addEventListener('click', function() { 
   const city = inputEl.value;
-
   // Perform direct geocoding API call to get latitude and longitude
   fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=1&appid=${apiKey}`)
     .then(response => response.json())
